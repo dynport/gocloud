c748cc43fc0690086c2943cc345ee9679e30daf6
Tobias Schwab <tobias.schwab@dynport.de>
2013-10-29 12:04:49 +0100

    add PUT method for S3, make endpoint variable

--- aws/s3/client.go
+++ aws/s3/client.go
@@ -1,10 +1,12 @@
 package s3
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/xml"
+	"fmt"
 	"github.com/dynport/gocloud/aws"
 	"hash"
 	"io/ioutil"
@@ -18,13 +20,19 @@ import (
 var b64 = base64.StdEncoding
 
 const (
-	ENDPOINT     = "https://s3.amazonaws.com"
-	CONTENT_MD5  = "Content-Md5"
-	CONTENT_TYPE = "Content-Type"
+	DEFAULT_ENDPOINT_HOST = "s3.amazonaws.com"
+	HEADER_CONTENT_MD5    = "Content-Md5"
+	HEADER_CONTENT_TYPE   = "Content-Type"
+	HEADER_DATE           = "Date"
+	HEADER_AUTHORIZATION  = "Authorization"
+	AMZ_ACL_PUBLIC        = "public-read"
+	DEFAULT_CONTENT_TYPE  = "application/octet-stream"
+	HEADER_AMZ_ACL        = "X-Amz-Acl"
 )
 
 type Client struct {
 	*aws.Client
+	CustomEndpointHost string
 }
 
 func NewFromEnv() *Client {
@@ -46,8 +54,67 @@ type ListAllMyBucketsResult struct {
 	Buckets []*Bucket `xml:"Buckets>Bucket"`
 }
 
+func (client *Client) EndpointHost() string {
+	if client.CustomEndpointHost != "" {
+		return client.CustomEndpointHost
+	}
+	return DEFAULT_ENDPOINT_HOST
+}
+
+func (client *Client) Endpoint() string {
+	return "https://" + client.EndpointHost()
+}
+
+type PutOptions struct {
+	ContentType string
+	AmzAcl      string
+}
+
+type Content struct {
+	Key              string    `xml:"Key"`
+	LastModified     time.Time `xml:"LastModified"`
+	Etag             string    `xml:"ETag"`
+	Size             int       `xml:"Size"`
+	StorageClass     string    `xml:"StorageClass"`
+	OwnerID          string    `xml:"Owner>ID"`
+	OwnerDisplayName string    `xml:"Owner>DisplayName"`
+}
+
+type ListBucketResult struct {
+	XMLName     xml.Name `xml:"ListBucketResult"`
+	Name        string   `xml:"Name"`
+	Prefix      string   `xml:"Prefix"`
+	Marker      string   `xml:"Marker"`
+	MaxKeys     int      `xml:"MaxKeys"`
+	IsTruncated bool     `xml:"IsTruncated"`
+
+	Contents []*Content `xml:"Contents"`
+}
+
+func (client *Client) ListBucket(bucket string) (r *ListBucketResult, e error) {
+	req, e := http.NewRequest("GET", "http://"+bucket+"."+client.EndpointHost()+"/", nil)
+	if e != nil {
+		return r, e
+	}
+	client.SignS3Request(req, bucket)
+	rsp, e := http.DefaultClient.Do(req)
+	if e != nil {
+		return r, e
+	}
+	b, e := ioutil.ReadAll(rsp.Body)
+	if e != nil {
+		return r, e
+	}
+	r = &ListBucketResult{}
+	e = xml.Unmarshal(b, r)
+	if e != nil {
+		return r, e
+	}
+	return r, e
+}
+
 func (client *Client) Service() (r *ListAllMyBucketsResult, e error) {
-	req, e := http.NewRequest("GET", ENDPOINT+"/", nil)
+	req, e := http.NewRequest("GET", client.Endpoint()+"/", nil)
 	if e != nil {
 		return r, e
 	}
@@ -56,7 +123,6 @@ func (client *Client) Service() (r *ListAllMyBucketsResult, e error) {
 	if e != nil {
 		return r, e
 	}
-	log.Println(rsp.Status)
 	defer rsp.Body.Close()
 	b, e := ioutil.ReadAll(rsp.Body)
 	if e != nil {
@@ -64,7 +130,58 @@ func (client *Client) Service() (r *ListAllMyBucketsResult, e error) {
 	}
 	r = &ListAllMyBucketsResult{}
 	e = xml.Unmarshal(b, r)
+	if e != nil {
+		return nil, e
+	}
 	return r, e
+}
+
+func (client *Client) Put(bucket, key string, data []byte, options *PutOptions) error {
+	log.Printf("uploading %s to %s/%s", data, bucket, key)
+	if options == nil {
+		options = &PutOptions{ContentType: DEFAULT_CONTENT_TYPE}
+	}
+
+	buf := &bytes.Buffer{}
+	buf.Write(data)
+	req, e := http.NewRequest("PUT", "http://"+bucket+"."+client.EndpointHost()+"/"+key, buf)
+	if e != nil {
+		return e
+	}
+
+	req.Header.Add("Host", bucket+"."+client.EndpointHost())
+
+	contentType := options.ContentType
+	if contentType == "" {
+		contentType = DEFAULT_CONTENT_TYPE
+	}
+	req.Header.Add(HEADER_CONTENT_TYPE, contentType)
+
+	if options.AmzAcl != "" {
+		req.Header.Add(HEADER_AMZ_ACL, options.AmzAcl)
+	}
+
+	b64md5, e := contentMd5(string(data))
+	if e != nil {
+		return e
+	}
+	req.Header.Add(HEADER_CONTENT_MD5, b64md5)
+
+	client.SignS3Request(req, bucket)
+	rsp, e := http.DefaultClient.Do(req)
+	if e != nil {
+		return e
+	}
+	defer rsp.Body.Close()
+	b, e := ioutil.ReadAll(rsp.Body)
+	if e != nil {
+		log.Println(string(b))
+		return e
+	}
+	if rsp.StatusCode != 200 {
+		return fmt.Errorf("error uploading key: %s - %s", rsp.Status, string(b))
+	}
+	return nil
 }
 
 func (client *Client) SignS3Request(req *http.Request, bucket string) {
@@ -72,8 +189,8 @@ func (client *Client) SignS3Request(req *http.Request, bucket string) {
 	date := t.Format(http.TimeFormat)
 	payloadParts := []string{
 		req.Method,
-		req.Header.Get(CONTENT_MD5),
-		req.Header.Get(CONTENT_TYPE),
+		req.Header.Get(HEADER_CONTENT_MD5),
+		req.Header.Get(HEADER_CONTENT_TYPE),
 		date,
 	}
 	amzHeaders := []string{}
@@ -91,8 +208,8 @@ func (client *Client) SignS3Request(req *http.Request, bucket string) {
 	}
 	payloadParts = append(payloadParts, path)
 	payload := strings.Join(payloadParts, "\n")
-	req.Header.Add("Date", date)
-	req.Header.Add("Authorization", "AWS "+client.Key+":"+signPayload(payload, client.newSha1Hash(client.Secret)))
+	req.Header.Add(HEADER_DATE, date)
+	req.Header.Add(HEADER_AUTHORIZATION, "AWS "+client.Key+":"+signPayload(payload, client.newSha1Hash(client.Secret)))
 }
 
 func (client *Client) newSha1Hash(secret string) hash.Hash {
