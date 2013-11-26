49769af66deb5cc9bc8e1ab033e64593bf53c514
Tobias Schwab <tobias.schwab@dynport.de>
2013-11-26 12:39:12 +0100

    remove placeholder

--- aws/ec2/pricing.go
+++
@@ -1,8 +0,0 @@
-package ec2
-
-// current instance pricings can be fetched from those urls
-//
-// http://aws.amazon.com/ec2/pricing/pricing-on-demand-instances.json
-// http://aws.amazon.com/ec2/pricing/ri-light-linux.json
-// http://aws.amazon.com/ec2/pricing/ri-medium-linux.json
-// http://aws.amazon.com/ec2/pricing/ri-heavy-linux.json
