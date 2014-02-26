7e8e01d1ccb408470e0d1b298ac273e6dfccc516
Tobias Schwab <tobias.schwab@dynport.de>
2014-02-26 22:45:02 +0100

    change ListVersions to new style

--- aws/s3/list_versions.go
+++ aws/s3/list_versions.go
@@ -2,8 +2,10 @@ package s3
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 type ListVersionsResult struct {
@@ -38,13 +40,61 @@ type Version struct {
 	StorageClass string `xml:"StorageClass"` // STANDARD</StorageClass>
 }
 
-func (client *Client) ListVersions(bucket string) (*ListVersionsResult, error) {
-	req, e := http.NewRequest("GET", "https://"+client.EndpointHost()+"/"+bucket+"?versions", nil)
+type ListVersions struct {
+	Bucket string
+
+	// for parameter
+	Delimiter       string
+	EncodingType    string
+	KeyMarker       string
+	MaxKeys         string
+	Prefix          string
+	VersionIdMarker string
+}
+
+func (a *ListVersions) query() string {
+	u := url.Values{}
+	if a.Delimiter != "" {
+		u.Add("delimiter", a.Delimiter)
+	}
+	if a.EncodingType != "" {
+		u.Add("encoding-type", a.EncodingType)
+	}
+	if a.KeyMarker != "" {
+		u.Add("key-marker", a.KeyMarker)
+	}
+	if a.MaxKeys != "" {
+		u.Add("max-keys", a.MaxKeys)
+	}
+	if a.Prefix != "" {
+		u.Add("prefix", a.Prefix)
+	}
+	if a.VersionIdMarker != "" {
+		u.Add("verdion-id-marker", a.VersionIdMarker)
+	}
+
+	if len(u) > 0 {
+		return u.Encode()
+	}
+	return ""
+}
+
+func (a *ListVersions) Execute(client *Client) (*ListVersionsResult, error) {
+	theUrl := "https://" + client.EndpointHost() + "/" + a.Bucket + "?versions"
+	q := a.query()
+	if q != "" {
+		theUrl += "&" + q
+	}
+	if a.Bucket == "" {
+		return nil, fmt.Errorf("Bucket must be set")
+	}
+
+	req, e := http.NewRequest("GET", theUrl, nil)
 	if e != nil {
 		return nil, e
 	}
-	req.Header.Add("Host", bucket+"."+client.EndpointHost())
-	client.SignS3Request(req, bucket)
+	req.Header.Add("Host", a.Bucket+"."+client.EndpointHost())
+	client.SignS3Request(req, a.Bucket)
 	rsp, e := http.DefaultClient.Do(req)
 	if e != nil {
 		return nil, e
