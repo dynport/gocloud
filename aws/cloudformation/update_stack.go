65c229bea7c5f5bd6e450dfe787151f24de14a4d
Tobias Schwab <tobias.schwab@dynport.de>
2014-04-07 18:20:21 +0200

    introduce method object for UpdateStack

--- aws/cloudformation/update_stack.go
+++ aws/cloudformation/update_stack.go
@@ -21,6 +21,39 @@ type UpdateStackResult struct {
 	StackId string `xml:"StackId"`
 }
 
+type UpdateStack struct {
+	Parameters                  []*StackParameter
+	Capabilities                []string
+	StackName                   string
+	StackPolicyBody             string
+	StackPolicyURL              string
+	TemplateBody                string
+	TemplateURL                 string
+	StackPolicyDuringUpdateBody string
+	StackPolicyDuringUpdateURL  string
+}
+
+func (update *UpdateStack) values() Values {
+	v := Values{
+		"StackName":                   update.StackName,
+		"StackPolicyBody":             update.StackPolicyBody,
+		"StackPolicyURL":              update.StackPolicyURL,
+		"TemplateBody":                update.TemplateBody,
+		"TemplateURL":                 update.TemplateURL,
+		"StackPolicyDuringUpdateBody": update.StackPolicyDuringUpdateBody,
+		"StackPolicyDuringUpdateURL":  update.StackPolicyDuringUpdateURL,
+	}
+	v.updateCapabilities(update.Capabilities)
+	v.updateParameters(update.Parameters)
+	return v
+}
+
+func (update *UpdateStack) Execute(client *Client) (*UpdateStackResponse, error) {
+	r := &UpdateStackResponse{}
+	e := client.loadCloudFormationResource("UpdateStack", update.values(), r)
+	return r, e
+}
+
 func (c *Client) UpdateStack(params UpdateStackParameters) (stackId string, e error) {
 	r := &UpdateStackResponse{}
 	e = c.loadCloudFormationResource("UpdateStack", params.values(), r)
