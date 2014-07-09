bce752f4dc46a0ed03f80a72cba47a68f1e513bd
Tobias Schwab <tobias.schwab@dynport.de>
2014-07-09 10:03:24 +0200

    add headers and ip address to listing droplets

--- cmd/digo2/droplets_list.go
+++ cmd/digo2/droplets_list.go
@@ -19,12 +19,17 @@ func (r *dropletsList) Run() error {
 		return e
 	}
 	t := gocli.NewTable()
+	t.Add("Id", "Status", "Ip", "Name", "Region", "Size", "ImageId:ImageName (ImageSlug)", "CreatedAt")
 	for _, d := range rsp.Droplets {
-		imageName := fmt.Sprintf("%d: %s", d.Image.Id, d.Image.Name)
+		imageName := fmt.Sprintf("%d:%s", d.Image.Id, d.Image.Name)
 		if d.Image.Slug != "" {
 			imageName += " (" + d.Image.Slug + ")"
 		}
-		t.Add(d.Id, d.Status, d.CreatedAt.Format("2006-01-02T15:04:05"), d.Name, d.Region.Name, d.Size.Slug, imageName)
+		ip := ""
+		if len(d.Networks.V4) > 0 {
+			ip = d.Networks.V4[0].IpAddress
+		}
+		t.Add(d.Id, d.Status, ip, d.Name, d.Region.Name, d.Size.Slug, imageName, d.CreatedAt.Format("2006-01-02T15:04:05"))
 	}
 	fmt.Println(t)
 	return nil
