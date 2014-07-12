b154d81cbc3ffab4f272662d8be739e53dffc3de
Tobias Schwab <tobias.schwab@dynport.de>
2014-07-12 21:33:40 +0200

    display region slug instead of name

--- cmd/digo2/droplets_list.go
+++ cmd/digo2/droplets_list.go
@@ -29,7 +29,7 @@ func (r *dropletsList) Run() error {
 		if len(d.Networks.V4) > 0 {
 			ip = d.Networks.V4[0].IpAddress
 		}
-		t.Add(d.Id, d.Status, ip, d.Name, d.Region.Name, d.Size.Slug, imageName, d.CreatedAt.Format("2006-01-02T15:04:05"))
+		t.Add(d.Id, d.Status, ip, d.Name, d.Region.Slug, d.Size.Slug, imageName, d.CreatedAt.Format("2006-01-02T15:04:05"))
 	}
 	fmt.Println(t)
 	return nil
